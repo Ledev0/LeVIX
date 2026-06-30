b9fe6a7db6ae6c2ea3c1a23be9512be0b541db67
Seif Amr <seifamr.dev@gmail.com>
2026-06-30 09:36:18 +0300

    fix: migrate treesitter config to main branch API
    highlight/indent options no longer apply via setup(); now enabled
    via FileType autocmd with vim.treesitter.start().

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,35 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "java", "python", "c", "cpp", "lua", "bash", "markdown", "markdown_inline" },
-		sync_install = false,
-		auto_install = true,
-		highlight = {
-			enable = true, -- Highlighting engine
-			additional_vim_regex_highlighting = false,
-		},
-		indent = { enable = true },
-	},
-	config = function(_, opts)
-		require("nvim-treesitter").setup(opts)
+	lazy = false,
+	config = function()
+		require("nvim-treesitter").setup({})
+
+		require("nvim-treesitter").install({
+			"java",
+			"python",
+			"c",
+			"cpp",
+			"lua",
+			"bash",
+			"markdown",
+			"markdown_inline",
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "*",
+			callback = function()
+				local ft = vim.bo.filetype
+				if ft and ft ~= "" then
+					pcall(vim.treesitter.start)
+					pcall(function()
+						vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+						vim.wo[0][0].foldmethod = "expr"
+						vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end)
+				end
+			end,
+		})
 	end,
 }
