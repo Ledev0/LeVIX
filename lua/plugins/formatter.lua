ebaf7db0a815f0220030a93fe9c9dcd74304c366
Seif Amr <seifamr.dev@gmail.com>
2026-06-30 09:40:13 +0300

    feat: add missing-tool detection with clear warnings for linters/formatters
    Prevents silent ENOENT failures; maps clangtidy linter id to
    actual clang-tidy executable name.

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -2,7 +2,9 @@ return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	config = function()
-		require("conform").setup({
+		local conform = require("conform")
+
+		conform.setup({
 			formatters_by_ft = {
 				java = { "google-java-format" },
 				python = { "ruff_format" },
@@ -10,14 +12,43 @@ return {
 				cpp = { "clang-format" },
 				lua = { "stylua" },
 			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+		})
+
+		local warned = {}
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function(args)
+				local ft = vim.bo[args.buf].filetype
+				local formatters = conform.formatters_by_ft[ft]
+				if not formatters then
+					return
+				end
+
+				for _, name in ipairs(formatters) do
+					local info = conform.get_formatter_info(name, args.buf)
+					if not info.available then
+						if not warned[name] then
+							warned[name] = true
+							vim.notify(
+								string.format(
+									"LeVIX: formatter '%s' not found. Install it to enable formatting for %s files.",
+									name,
+									ft
+								),
+								vim.log.levels.WARN,
+								{ title = "LeVIX Formatter" }
+							)
+						end
+						return
+					end
+				end
+
+				conform.format({ bufnr = args.buf, timeout_ms = 500, lsp_fallback = true })
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>cf", function()
-			require("conform").format({ async = true })
+			conform.format({ async = true })
 		end, { desc = "Format File" })
 	end,
 }
--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -11,14 +11,44 @@ return {
 			cpp = { "clangtidy" },
 		}
 
+		local executable_map = {
+			clangtidy = "clang-tidy",
+		}
+
+		local warned = {}
+
+		local function try_lint_safe()
+			local ft = vim.bo.filetype
+			local linters = lint.linters_by_ft[ft]
+			if not linters then
+				return
+			end
+
+			for _, name in ipairs(linters) do
+				local exe = executable_map[name] or name
+				if vim.fn.executable(exe) == 0 then
+					if not warned[name] then
+						warned[name] = true
+						vim.notify(
+							string.format(
+								"LeVIX: linter '%s' not found in PATH. Install it to enable linting for %s files.",
+								exe,
+								ft
+							),
+							vim.log.levels.WARN,
+							{ title = "LeVIX Linter" }
+						)
+					end
+					return
+				end
+			end
+
+			lint.try_lint()
+		end
 		vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
-			callback = function()
-				lint.try_lint()
-			end,
+			callback = try_lint_safe,
 		})
 
-		vim.keymap.set("n", "<leader>cl", function()
-			lint.try_lint()
-		end, { desc = "Lint File" })
+		vim.keymap.set("n", "<leader>cl", try_lint_safe, { desc = "Lint File" })
 	end,
 }
