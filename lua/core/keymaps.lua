f9a45b4ec945d594574e2e11b7cf30ec31630ebc
Seif Amr <seifamr.dev@gmail.com>
2026-06-30 09:33:23 +0300

    fix: remove duplicate <C-hjkl> and <Esc> keymap definitions

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,8 +1,6 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-keymap("n", "<Esc>", "<cmd>nohlsearch<CR>", opts)
-
 -- Fast Saving / Quiting
 keymap("n", "<leader>w", "<cmd>write<CR>", { desc = "Save file" })
 keymap("n", "<leader>q", "<cmd>quit<CR>", { desc = "Quit" })
@@ -46,10 +44,4 @@ keymap("n", "<leader>gl", "<cmd>lua require('gitsigns').blame_line()<CR>", { des
 keymap("n", "<leader>gp", "<cmd>lua require('gitsigns').preview_hunk()<CR>", { desc = "Preview Change Hunk" })
 keymap("n", "<leader>gr", "<cmd>lua require('gitsigns').reset_hunk()<CR>", { desc = "Reset Line Change" })
 -- Lazygit integrated through ToggleTerm
-keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", { desc = "Toggle Lazygit" })
-
--- Navigation between splits
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
+keymap("n", "<leader>gg", "<cmd>lua LeVIX.lazygit_toggle()<CR>", { desc = "Toggle Lazygit" })
