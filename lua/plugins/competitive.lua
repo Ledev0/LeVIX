48f3530f34190069d4209bf159225f30cb9174b6
Seif Amr <seifamr.dev@gmail.com>
2026-07-12 03:38:15 +0300

    Change : competitivw.lua and java-dap.lua fromat

--- lua/plugins/competitive.lua
+++ lua/plugins/competitive.lua
@@ -5,15 +5,21 @@ return {
 		config = function()
 			require("code_runner").setup({
 				filetype = {
-					java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
-					python = "python3 $file",
-					c = "cd $dir && gcc -Wall -g $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
-					cpp = "cd $dir && g++ -Wall -g -std=c++17 $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+					java = "cd $dir && javac $fileName && java $fileNameWithoutExt < input.txt",
+					python = "python3 $file < input.txt",
+					c = "cd $dir && gcc -Wall -g $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt < input.txt",
+					cpp = "cd $dir && g++ -Wall -g -std=c++17 $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt < input.txt",
 				},
 			})
 
 			vim.keymap.set("n", "<leader>rc", "<cmd>RunCode<CR>", { desc = "Run Code" })
 			vim.keymap.set("n", "<leader>rf", "<cmd>RunFile<CR>", { desc = "Run File" })
+
+			vim.keymap.set("n", "<leader>ri", function()
+				local dir = vim.fn.expand("%:p:h")
+				local input_path = dir .. "/input.txt"
+				vim.cmd("botright 10split " .. vim.fn.fnameescape(input_path))
+			end, { desc = "Open Input File" })
 		end,
 	},
 }
--- lua/plugins/java-dap.lua
+++ lua/plugins/java-dap.lua
@@ -2,20 +2,26 @@ return {
 	"mfussenegger/nvim-jdtls",
 	ft = "java",
 	config = function()
-		local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+		local function setup_jdtls()
+			local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+			local workspace_dir = vim.fn.stdpath("data") .. "/jdtls-workspace/" .. project_name
 
-		local workspace_dir = vim.fn.stdpath("data") .. "/jdtls-workspace/" .. project_name
+			local config = {
+				cmd = { "jdtls", "-data", workspace_dir },
+				root_dir = vim.fs.dirname(vim.fs.find({ ".git", "mvnw", "gradlew", "pom.xml" }, { upward = true })[1]),
+				capabilities = require("blink.cmp").get_lsp_capabilities(),
+				on_attach = function(client, bufnr)
+					require("jdtls").setup_dap({ hotcodereplace = "auto" })
+				end,
+			}
 
-		local config = {
-			cmd = { "jdtls", "-data", workspace_dir },
-
-			root_dir = vim.fs.dirname(vim.fs.find({ ".git", "mvnw", "gradlew", "pom.xml" }, { upward = true })[1]),
-		}
-
-		config["on_attach"] = function(client, bufnr)
-			require("jdtls").setup_dap({ hotcodereplace = "auto" })
+			require("jdtls").start_or_attach(config)
 		end
 
-		require("jdtls").start_or_attach(config)
+		setup_jdtls()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "java",
+			callback = setup_jdtls,
+		})
 	end,
 }
