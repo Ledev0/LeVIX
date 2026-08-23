dfb3fb6576f0d6e530618cdb2aecf99a7079ebc8
Seif Amr <seifamr.dev@gmail.com>
2026-08-23 04:44:43 +0300

    Add : Restart Keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,10 +1,10 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
--- Fast Saving / Quiting
+-- Fast Saving , Quiting and Restart
 keymap("n", "<leader>w", "<cmd>write<CR>", { desc = "Save file" })
 keymap("n", "<leader>q", "<cmd>quit<CR>", { desc = "Quit" })
-
+keymap("n", "<leader>R", "<cmd>restart<CR>", { desc = "Restart" })
 -- Splits Moving
 keymap("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
 keymap("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
@@ -25,10 +25,10 @@ keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
 keymap("v", "p", '"_dP', opts)
 
 -- Buffers
-keymap("n", "<leader>i", ":enew<CR>", { desc = "New Buffer" })
-keymap("n", "<S-l>", ":bnext<CR>", { desc = "Next Buffer" })
-keymap("n", "<S-h>", ":bprevious<CR>", { desc = "Previous Buffer" })
-keymap("n", "<leader>x", ":bdelete<CR>", { desc = "Close current buffer" })
+keymap("n", "<leader>v", "<cmd>enew<CR>", { desc = "New Buffer" })
+keymap("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next Buffer" })
+keymap("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous Buffer" })
+keymap("n", "<leader>x", "<cmd>bdelete<CR>", { desc = "Close current buffer" })
 
 -- Toggleterm bindings
 keymap("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", { desc = "Terminal Horizontal" })
