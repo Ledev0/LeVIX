6c9652c1d30fc1ceb8ac44637b1bbb396ceb9a36
Seif Amr <seifamr.dev@gmail.com>
2026-08-27 00:34:00 +0300

    Refactor : move theme picker to keymaps.lua
    Move theme_cache, save_theme, and <leader>T keymap from
    themes.lua init function to core/keymaps.lua. themes.lua
    now only declares theme plugins.

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,5 +1,3 @@
-local theme_cache = vim.fn.stdpath("config") .. "/.levix_theme_cache"
-
 return {
 	{ "catppuccin/nvim", name = "catppuccin", lazy = true },
 	{ "folke/tokyonight.nvim", lazy = true },
@@ -8,57 +6,6 @@ return {
 	{ "rebelot/kanagawa.nvim", lazy = true },
 	{ "xero/miasma.nvim", lazy = true },
 	{ "aliqyan-21/darkvoid.nvim", lazy = true },
-    { "dasupradyumna/midnight.nvim", lazy = false, },
-    { "tanvirtin/monokai.nvim", lazy = false },
-	{
-		"folke/which-key.nvim",
-		init = function()
-			local function save_theme(theme)
-				local file = io.open(theme_cache, "w")
-				if file then
-					file:write(theme)
-					file:close()
-				end
-			end
-
-			vim.keymap.set("n", "<leader>T", function()
-				local success, builtin = pcall(require, "telescope.builtin")
-
-				if success then
-					builtin.colorscheme({
-						enable_preview = true,
-						previewer = false,
-						layout_strategy = "center",
-						layout_config = {
-							width = 50,
-							height = 20,
-							prompt_position = "top",
-							preview_cutoff = 1,
-						},
-						attach_mappings = function(_, map)
-							map("i", "<CR>", function(prompt_bufnr)
-								local actions = require("telescope.actions")
-								local action_state = require("telescope.actions.state")
-								local selection = action_state.get_selected_entry()
-								actions.close(prompt_bufnr)
-								if selection then
-									pcall(vim.cmd, "colorscheme " .. selection.value)
-									save_theme(selection.value)
-								end
-							end)
-							return true
-						end,
-					})
-				else
-					local themes = { "catppuccin", "tokyonight", "gruvbox", "rose-pine", "kanagawa" }
-					vim.ui.select(themes, { prompt = "  Select LeVIX Theme:" }, function(choice)
-						if choice then
-							pcall(vim.cmd, "colorscheme " .. choice)
-							save_theme(choice)
-						end
-					end)
-				end
-			end, { desc = "Switch Theme" })
-		end,
-	},
+	{ "dasupradyumna/midnight.nvim", lazy = false },
+	{ "tanvirtin/monokai.nvim", lazy = false },
 }
