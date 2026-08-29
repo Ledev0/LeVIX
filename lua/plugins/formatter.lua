dab86a50fc8942b46c5d6d3d36b70ce9b8b0f93e
Seif Amr <seifamr.dev@gmail.com>
2026-08-29 23:39:09 +0300

    fix: load conform at VeryLazy, no early return

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,55 +1,57 @@
 return {
-	"stevearc/conform.nvim",
-	event = "VeryLazy",
-	config = function()
-		local conform = require("conform")
+	{
+		"stevearc/conform.nvim",
+		event = "VeryLazy",
+		config = function()
+			local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				java = { "google-java-format" },
-				python = { "ruff_format" },
-				c = { "clang-format" },
-				cpp = { "clang-format" },
-				lua = { "stylua" },
-				html = { "prettier" },
-				css = { "prettier" },
-				javascript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-			},
-		})
+			conform.setup({
+				formatters_by_ft = {
+					java = { "google-java-format" },
+					python = { "ruff_format" },
+					c = { "clang-format" },
+					cpp = { "clang-format" },
+					lua = { "stylua" },
+					html = { "prettier" },
+					css = { "prettier" },
+					javascript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescript = { "prettier" },
+					typescriptreact = { "prettier" },
+				},
+			})
 
-		local warned = {}
+			local warned = {}
 
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			callback = function(args)
-				local ft = vim.bo[args.buf].filetype
-				local formatters = conform.formatters_by_ft[ft]
-				if not formatters then
-					return
-				end
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				callback = function(args)
+					local ft = vim.bo[args.buf].filetype
+					local formatters = conform.formatters_by_ft[ft]
+					if not formatters then
+						return
+					end
 
-				for _, name in ipairs(formatters) do
-					local info = conform.get_formatter_info(name, args.buf)
-					if not info.available then
-						if not warned[name] then
-							warned[name] = true
-							vim.notify(
-								string.format(
-									"LeVIX: formatter '%s' not found. Install it to enable formatting for %s files.",
-									name,
-									ft
-								),
-								vim.log.levels.WARN,
-								{ title = "LeVIX Formatter" }
-							)
+					for _, name in ipairs(formatters) do
+						local info = conform.get_formatter_info(name, args.buf)
+						if not info.available then
+							if not warned[name] then
+								warned[name] = true
+								vim.notify(
+									string.format(
+										"LeVIX: formatter '%s' not found. Install it to enable formatting for %s files.",
+										name,
+										ft
+									),
+									vim.log.levels.WARN,
+									{ title = "LeVIX Formatter" }
+								)
+							end
 						end
 					end
-				end
 
-				conform.format({ bufnr = args.buf, timeout_ms = 3000, lsp_fallback = true })
-			end,
-		})
-	end,
+					conform.format({ bufnr = args.buf, timeout_ms = 3000, lsp_fallback = true })
+				end,
+			})
+		end,
+	},
 }
